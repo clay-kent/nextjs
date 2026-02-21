1525f24981c3bd0f5d1c81543aa3a9f4f5c991b0
白居健音 <152370128+clay-kent@users.noreply.github.com>
2026-02-21 15:19:52 +0000

    fix: update allowed tags in sanitizeHTML to include list elements

--- src/app/_utils/sanitizeHTML.ts
+++ src/app/_utils/sanitizeHTML.ts
@@ -18,6 +18,10 @@ export const sanitizeHTML = (html: string) => {
       "pre",
       "code",
       "span",
+      "ul",
+      "ol",
+      "li",
+      "a",
     ],
     ALLOWED_ATTR: ["src", "alt", "width", "height", "loading", "class"],
     // svgを除外し、主要な静止画フォーマットのみに限定
